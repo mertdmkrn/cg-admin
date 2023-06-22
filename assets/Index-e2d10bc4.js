3bd0244225f1f34a0dc7224c8bef7215800a9236
mertdmkrn37 <mertdmkrn37@gmail.com>
2023-06-22 13:35:30 +0300

    Düzenlemeler.

---
+++ assets/Index-e2d10bc4.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index-c7f8cf53.js";const o={},t={class:"row"};function r(s,a){return c(),n("div",t," Payment Page Under Construction... ")}const d=e(o,[["render",r]]);export{d as default};
