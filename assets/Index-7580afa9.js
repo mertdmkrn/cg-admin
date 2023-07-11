e6a1c302dab77ba31b35d8c07689ff820dae4269
mertdmkrn37 <mertdmkrn37@gmail.com>
2023-07-11 11:19:39 +0300

    Profil sayfası ve admin ekleme sayfası eklendi.

--- assets/Index-7580afa9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-297fa9c7.js";const n={},r={class:"row"};function s(t,_){return c(),o("div",r," User Page Under Construction... ")}const d=e(n,[["render",s]]);export{d as default};
