ae377c71bde58dca26ec05a7e0685d9e2d48c173
mertdmkrn37 <mertdmkrn37@gmail.com>
2023-09-17 00:36:01 +0300

    Business sayfası düzenlemeleri.

--- assets/Index-c947c3da.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index-13d211b1.js";const o={},t={class:"row"};function r(s,a){return c(),n("div",t," Payment Page Under Construction... ")}const d=e(o,[["render",r]]);export{d as default};
