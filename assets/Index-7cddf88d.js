14a9b7cdbd7a0acc1aa0e69f46326b36b6477d42
mertdmkrn37 <mertdmkrn37@gmail.com>
2023-09-23 15:31:53 +0300

    Business düzenleme sayfasına ayarlar tabı eklendi.

---
+++ assets/Index-7cddf88d.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-8f291531.js";const n={};function r(t,c){return" Payment Page Under Construction... "}const _=e(n,[["render",r]]);export{_ as default};
--- assets/Index-8a843100.js
+++
@@ -1,1 +0,0 @@
-import{_ as n}from"./index-22478e82.js";const e={};function r(t,c){return" Rating Page Under Construction... "}const _=n(e,[["render",r]]);export{_ as default};
