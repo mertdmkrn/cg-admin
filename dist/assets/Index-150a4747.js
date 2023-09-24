25ec99dd661664c660ba5767b68d6c8591905adb
mertdmkrn37 <mertdmkrn37@gmail.com>
2023-09-24 13:07:46 +0300

    Business sayfasına filtreleme eklendi.

---
+++ dist/assets/Index-150a4747.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-37ec9d54.js";const n={};function r(t,c){return" Payment Page Under Construction... "}const _=e(n,[["render",r]]);export{_ as default};
--- src/views/Business/Index.vue
+++ src/views/Business/Index.vue
@@ -1,4 +1,49 @@
 <template>
+    <details style="margin-bottom: 10px;">
+        <summary>Filter</summary>
+        <div class="form-group-sm row p-3" style="border: 1px solid #e3e6f0; border-radius: 1px; width:99.95%; margin-left:0.025%">
+            <div class="col-md-3">
+                <label class="col-form-label-sm" for="name">Full Name</label>
+                <input type="text" v-model="searchModel.name" class="form-control form-control-sm" id="name">
+            </div>
+            <div class="col-md-3">
+                <label class="col-form-label-sm" for="city">City</label>
+                <ModelListSelect
+                    :list="this.cities"
+                    v-model="searchModel.city"
+                    id="city"
+                    option-value="value"
+                    option-text="name"
+                    class="form-control form-control-sm"
+                    placeholder="Select City">
+                </ModelListSelect>            
+            </div>
+            <div class="col-md-3">
+                <label class="col-form-label-sm" for="workingGenderType">Gender Type</label>
+                <select class="form-control form-control-sm" v-model="searchModel.workingGenderType">
+                    <option v-for="(item, index) in this.workingGenderTypeList" :key="item" :value="index">
+                        {{ item }}
+                    </option>
+                </select>                     
+            </div>
+            <div class="col-md-2">              
+                <div class="form-check form-switch ml-4 mt-2">
+                    <input class="form-check-input form-check-input-sm" @change="searchModel.isOnlyNotActive = false" v-model="searchModel.isOnlyActive" type="checkbox" id="isOnlyActive">
+                    <label class="form-check-label col-form-label-sm" for="isOnlyActive">Only Active</label>
+                </div>           
+                <div class="form-check form-switch ml-4">
+                    <input class="form-check-input form-check-input-sm" @change="searchModel.isOnlyActive = false" v-model="searchModel.isOnlyNotActive" type="checkbox" id="isOnlyNotActive">
+                    <label class="form-check-label col-form-label-sm" for="isOnlyNotActive">Only Not Active</label>
+                </div>                     
+            </div>
+            <div class="col-md-1 mt-4 d-none d-lg-flex">
+                <a @click="this.getBusinesses()" v-if="!saveLoading" class="btn btn-sm m-2 btn-primary">Search</a>
+            </div>
+            <div class="col-md-12 mt-1 d-flex d-lg-none">
+                <a @click="this.getBusinesses()" v-if="!saveLoading" class="btn d-block btn-sm m-2 btn-primary" style="width: 105%;">Search</a>
+            </div>
+        </div>
+    </details>
     <div class="table-responsive">
         <table class="table table-sm table-bordered table-striped">
             <thead>
@@ -99,19 +144,34 @@ export default {
                 take: null,
                 name: null,
                 city: null,
-                workingGenderType: 4,
+                workingGenderType: 3,
                 isOnlyActive: false,
                 isOnlyNotActive: false
             },
             isLoading: true,
             saveLoading: false,
-            workingGenderTypeList: ["Everyone", "Female", "Male", "All"]
+            workingGenderTypeList: ["Everyone", "Female", "Male", "All"],
+            cities: []
         }
     },
     created() {
         this.getBusinesses();
+        this.getCities();
     },
     methods: {
+        async getCities()
+        {
+            this.isLoading = true;
+            await this.$appAxios.post("/definition/getcities").then(response => {
+                this.cities = response.data.data.map(cityName => {
+                    return {
+                        name: cityName,
+                        value: cityName
+                    };
+                });
+                this.isLoading = false;
+            }).catch(e => { alert(e.message); });
+        },
         async getBusinesses()
         {
             this.businesses = [];
@@ -119,9 +179,11 @@ export default {
             this.searchModel.page = this.page - 1;
             this.searchModel.take = this.perPage;
 
+            debugger;
+
             await this.$appAxios.post("/admin/getbusinesssearch", this.searchModel, {headers: { 'Authorization': `Bearer ${this._token}` }}).then(response => {
                 this.businesses = response.data.data;
-                this.records = this.businesses[0].itemCount;
+                this.records = this.businesses.length > 0 ? this.businesses[0].itemCount : 0;
                 this.isLoading = false;
             }).catch(e => { alert(e.message); });
         },
