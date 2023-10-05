04971a503e23cc2ae12c811c23182d24dd9d799a
mertdmkrn37 <mertdmkrn37@gmail.com>
2023-10-05 09:56:50 +0300

    Menüsel düzenlemeler yapıldı.

---
+++ assets/Index-58e7b0e9.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-fc5de74d.js";const n={};function r(t,c){return" Payment Page Under Construction... "}const _=e(n,[["render",r]]);export{_ as default};
---
+++ assets/Index-ad7794c7.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./index-fc5de74d.js";const e={};function r(t,c){return" Rating Page Under Construction... "}const _=n(e,[["render",r]]);export{_ as default};
