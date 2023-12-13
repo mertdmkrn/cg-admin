e39bcae4b5bc9ad13d245bb2dc4b79ff4fa20bf9
mertdmkrn37 <mertdmkrn37@gmail.com>
2023-12-13 14:36:47 +0300

    Api base url düzenlemesi.

--- src/utils/appAxios.js
+++ src/utils/appAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const appAxios = axios.create({
-    baseURL: "https://caregarden-71817683236b.herokuapp.com",
+    baseURL: "https://caregardenapi-095828432d4f.herokuapp.com",
     headers: {
       "Content-Type": "application/json",
       "Language" : "tr"
