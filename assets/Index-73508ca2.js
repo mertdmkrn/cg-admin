09a584774d7b9295e7f12b3b71426c9fd8aa3012
pc <pc@DESKTOP-86M3HVC>
2024-04-07 16:23:31 +0300

    Kampanya sayfasında düzenlemeler.

--- assets/Index-73508ca2.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index-aa7d2c7e.js";const e={};function n(t,c){return" Dashboard Page Under Construction... "}const s=r(e,[["render",n]]);export{s as default};
---
+++ assets/Index-76315bb1.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-0a614fd7.js";const n={};function r(t,c){return" Payment Page Under Construction... "}const _=e(n,[["render",r]]);export{_ as default};
