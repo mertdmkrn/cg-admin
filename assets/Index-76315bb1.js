55a050bb1d83d2ec17896f05e47b46a28b1e3965
pc <pc@DESKTOP-86M3HVC>
2024-04-25 17:20:40 +0300

    Api değişikliği.

--- assets/Index-76315bb1.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-0a614fd7.js";const n={};function r(t,c){return" Payment Page Under Construction... "}const _=e(n,[["render",r]]);export{_ as default};
---
+++ assets/Index-f5dcb9d5.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-81c29c83.js";const n={};function r(t,c){return" Payment Page Under Construction... "}const _=e(n,[["render",r]]);export{_ as default};
